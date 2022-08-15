0b6b3729af1ddeb58d1180c5f51f4baf16688ceb
Samuel Thompson <thomps9012@gmail.com>
2022-08-15 18:59:55 -0400

    updated main functions

--- main.go
+++ main.go
@@ -2,18 +2,20 @@ package main
 
 import (
 	"context"
-	"encoding/gob"
-	"encoding/json"
+	"crypto/rand"
+	"encoding/base64"
 	conn "financial-api/db"
 	c "financial-api/graphql/check_api"
 	m "financial-api/graphql/mileage_api"
 	p "financial-api/graphql/petty_api"
 	u "financial-api/graphql/user_api"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	"time"
 
-	"github.com/gorilla/sessions"
 	"github.com/graphql-go/graphql"
 	"github.com/graphql-go/handler"
 	"golang.org/x/oauth2"
@@ -40,24 +42,26 @@ var checkRequestSchema, _ = graphql.NewSchema(graphql.SchemaConfig{
 })
 
 var (
-	store       = sessions.NewCookieStore([]byte(os.Getenv("COOKIE_SECRET")), []byte(os.Getenv("COOKIE_ENCRYPT")))
+	// store       = sessions.NewCookieStore([]byte(os.Getenv("COOKIE_SECRET")), []byte(os.Getenv("COOKIE_ENCRYPT")))
 	oauthConfig = &oauth2.Config{
 		ClientID:     os.Getenv("GOOGLE_OAUTH_ID"),
 		ClientSecret: os.Getenv("GOOGLE_OAUTH_SECRET"),
 		Endpoint:     google.Endpoint,
 		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
-		RedirectURL:  "http://" + os.Getenv("HEROKU_APP_NAME") + "herokuapp.com/auth/google/callback",
+		RedirectURL:  "https://" + os.Getenv("HEROKU_APP_NAME") + "herokuapp.com/auth/google/callback",
 	}
 
-	stateToken = os.Getenv("HEROKU_APP_NAME")
+	// stateToken = os.Getenv("HEROKU_APP_NAME")
 )
 
-func init() {
-	gob.Register(&oauth2.Token{})
+const oauthGoogleUrlAPI = "https://www.googleapis.com/oauth2/v2/userinfo?access_token="
 
-	store.MaxAge(60 * 60 * 8)
-	store.Options.Secure = true
-}
+// func init() {
+// 	gob.Register(&oauth2.Token{})
+
+// 	store.MaxAge(60 * 60 * 8)
+// 	store.Options.Secure = true
+// }
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, `<html><body><a href="/auth/google">Sign In with Google</a></body></html>`)
@@ -65,62 +69,107 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 
 // edit this
 func handleAuth(w http.ResponseWriter, r *http.Request) {
-	url := oauthConfig.AuthCodeURL(stateToken)
-	http.Redirect(w, r, url, http.StatusFound)
+	oauthState := generateOauthCookie(w)
+	u := oauthConfig.AuthCodeURL(oauthState)
+	http.Redirect(w, r, u, http.StatusTemporaryRedirect)
+}
+
+func generateOauthCookie(w http.ResponseWriter) string {
+	var expiration = time.Now().Add(20 * time.Minute)
+
+	b := make([]byte, 16)
+	rand.Read(b)
+	state := base64.URLEncoding.EncodeToString(b)
+	cookie := http.Cookie{Name: "tundra-oauth", Value: state, Expires: expiration}
+	http.SetCookie(w, &cookie)
+
+	return state
 }
 
 func handleAuthCallback(w http.ResponseWriter, r *http.Request) {
-	if v := r.FormValue("state"); v != stateToken {
-		http.Error(w, "Invalid State token", http.StatusBadRequest)
+	oauthState, _ := r.Cookie("tundra-oauth")
+	if r.FormValue("state") != oauthState.Value {
+		log.Println("invalid oauth google state")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
 	}
-	ctx := context.Background()
-	token, err := oauthConfig.Exchange(ctx, r.FormValue("code"))
+
+	data, err := handleUser(r.FormValue("code"))
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Println(err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
 	}
-	session, err := store.Get(r, "tundra-oauth")
+	fmt.Fprintf(w, `<html><body><h1>Hello %s</h1><br /><h2>Below are some of the available APIs for the application</h2><br/><a href="/user_api">User Management API</a><br/><a href="/mileage_api">Mileage Request API</a><br/><a href="/petty_cash_api">Petty Cash API</a><br/><a href="/check_request_api">Check Request API</a></body></html>`, data)
+	// 	if v := r.FormValue("state"); v != stateToken {
+	// 		http.Error(w, "Invalid State token", http.StatusBadRequest)
+	// 		return
+	// 	}
+	// 	ctx := context.Background()
+	// 	token, err := oauthConfig.Exchange(ctx, r.FormValue("code"))
+	// 	if err != nil {
+	// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 		return
+	// 	}
+	// 	session, err := store.Get(r, "tundra-oauth")
+	// 	if err != nil {
+	// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 		return
+	// 	}
+	// 	session.Values["tundra-oauth"] = token
+	// 	if err := session.Save(r, w); err != nil {
+	// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 		return
+	// 	}
+	// http.Redirect(w, r, "/user", http.StatusFound)
+}
+func handleUser(code string) ([]byte, error) {
+	// Use code to get token and get user info from Google.
+
+	token, err := oauthConfig.Exchange(context.Background(), code)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return nil, fmt.Errorf("code exchange wrong: %s", err.Error())
 	}
-	session.Values["tundra-oauth"] = token
-	if err := session.Save(r, w); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+	response, err := http.Get(oauthGoogleUrlAPI + token.AccessToken)
+	if err != nil {
+		return nil, fmt.Errorf("failed getting user info: %s", err.Error())
 	}
-	http.Redirect(w, r, "/user", http.StatusFound)
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed read response: %s", err.Error())
+	}
+	return contents, nil
 }
 
-func handleUser(w http.ResponseWriter, r *http.Request) {
-	session, err := store.Get(r, "tundra-oauth")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	token, ok := session.Values["tundra-oauth"].(*oauth2.Token)
-	if !ok {
-		http.Error(w, "Unable to assert token", http.StatusInternalServerError)
-		return
-	}
-	client := oauthConfig.Client(context.Background(), token)
-	resp, err := client.Get("https://www.googleapis.com/auth/userinfo.email")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer resp.Body.Close()
-	d := json.NewDecoder(resp.Body)
-	var account struct {
-		Email string `json:"email"`
-	}
-	if err := d.Decode(&account); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	fmt.Fprintf(w, `<html><body><h1>Hello %s</h1><br /><h2>Below are some of the available APIs for the application</h2><br/><a href="/user_api">User Management API</a><br/><a href="/mileage_api">Mileage Request API</a><br/><a href="/petty_cash_api">Petty Cash API</a><br/><a href="/check_request_api">Check Request API</a></body></html>`, account.Email)
-}
+// func handleUser(w http.ResponseWriter, r *http.Request) {
+// 	session, err := store.Get(r, "tundra-oauth")
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		return
+// 	}
+// 	token, ok := session.Values["tundra-oauth"].(*oauth2.Token)
+// 	if !ok {
+// 		http.Error(w, "Unable to assert token", http.StatusInternalServerError)
+// 		return
+// 	}
+// 	client := oauthConfig.Client(context.Background(), token)
+// 	resp, err := client.Get("https://www.googleapis.com/auth/userinfo.email")
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		return
+// 	}
+// 	defer resp.Body.Close()
+// 	d := json.NewDecoder(resp.Body)
+// 	var account struct {
+// 		Email string `json:"email"`
+// 	}
+// 	if err := d.Decode(&account); err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		return
+// 	}
+// 	fmt.Fprintf(w, `<html><body><h1>Hello %s</h1><br /><h2>Below are some of the available APIs for the application</h2><br/><a href="/user_api">User Management API</a><br/><a href="/mileage_api">Mileage Request API</a><br/><a href="/petty_cash_api">Petty Cash API</a><br/><a href="/check_request_api">Check Request API</a></body></html>`, account.Email)
+// }
 
 func main() {
 	port := os.Getenv("PORT")
@@ -156,7 +205,7 @@ func main() {
 	http.HandleFunc("/", handleRoot)
 	http.HandleFunc("/auth/google", handleAuth)
 	http.HandleFunc("/auth/google/callback", handleAuthCallback)
-	http.HandleFunc("/user", handleUser)
+	// http.HandleFunc("/user", handleUser)
 	http.Handle("/user_api", userHandler)
 	http.Handle("/mileage_api", mileageHandler)
 	http.Handle("/petty_cash_api", pettyCashHandler)
