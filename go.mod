463836c0511d5b4a3f51fc281049c1e8589094a1
Samuel Thompson <thomps9012@gmail.com>
2022-08-15 21:57:09 -0400

    deploying test apis

--- go.mod
+++ go.mod
@@ -6,25 +6,15 @@ require (
 	github.com/google/uuid v1.3.0
 	github.com/graphql-go/graphql v0.8.0
 	go.mongodb.org/mongo-driver v1.10.1
-	golang.org/x/oauth2 v0.0.0-20220808172628-8227340efae7
 )
 
 require (
-	cloud.google.com/go/compute v1.7.0 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/gorilla/context v1.1.1 // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/gorilla/securecookie v1.1.1 // indirect
-	github.com/markbates/goth v1.73.0 // indirect
-	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
-	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/stretchr/testify v1.7.1 // indirect
 )
 
 require (
 	github.com/golang/snappy v0.0.3 // indirect
-	github.com/gorilla/pat v1.0.1
-	github.com/gorilla/sessions v1.2.1
 	github.com/graphql-go/handler v0.2.3
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
--- main.go
+++ main.go
@@ -6,17 +6,11 @@ import (
 	m "financial-api/graphql/mileage_api"
 	p "financial-api/graphql/petty_api"
 	u "financial-api/graphql/user_api"
-	"fmt"
-	"html/template"
 	"net/http"
 	"os"
 
-	"github.com/gorilla/sessions"
 	"github.com/graphql-go/graphql"
 	"github.com/graphql-go/handler"
-	"github.com/markbates/goth"
-	"github.com/markbates/goth/gothic"
-	"github.com/markbates/goth/providers/google"
 )
 
 const defaultPort = "8080"
@@ -38,107 +32,27 @@ var checkRequestSchema, _ = graphql.NewSchema(graphql.SchemaConfig{
 	Mutation: c.CheckRequestMutations,
 })
 
-// var (
-
-// 	oauthConfig = &oauth2.Config{
-// 		ClientID:     os.Getenv("GOOGLE_OAUTH_ID"),
-// 		ClientSecret: os.Getenv("GOOGLE_OAUTH_SECRET"),
-// 		Endpoint:     google.Endpoint,
-// 		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
-// 		RedirectURL:  "https://" + os.Getenv("HEROKU_APP_NAME") + "herokuapp.com/auth/google/callback",
-// 	}
-// 	oauthGoogleUrlAPI = "https://www.googleapis.com/oauth2/v2/userinfo?access_token="
-// )
-
-// func handleRoot(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprint(w, `<html><body><a href="/auth/google">Sign In with Google</a></body></html>`)
-// }
-
-// func handleAuth(w http.ResponseWriter, r *http.Request) {
-// 	oauthState := generateOauthCookie(w)
-// 	u := oauthConfig.AuthCodeURL(oauthState)
-// 	http.Redirect(w, r, u, http.StatusTemporaryRedirect)
-// }
-
-// func generateOauthCookie(w http.ResponseWriter) string {
-// 	var expiration = time.Now().Add(20 * time.Minute)
-
-// 	b := make([]byte, 16)
-// 	rand.Read(b)
-// 	state := base64.URLEncoding.EncodeToString(b)
-// 	cookie := http.Cookie{Name: "tundra-oauth", Value: state, Expires: expiration}
-// 	http.SetCookie(w, &cookie)
-
-// 	return state
-// }
-
-// func handleAuthCallback(w http.ResponseWriter, r *http.Request) {
-// 	oauthState, _ := r.Cookie("tundra-oauth")
-// 	if r.FormValue("state") != oauthState.Value {
-// 		log.Println("invalid oauth google state")
-// 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-// 		return
-// 	}
-
-// 	data, err := handleUser(r.FormValue("code"))
-// 	if err != nil {
-// 		log.Println(err.Error())
-// 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-// 		return
-// 	}
-// 	fmt.Fprintf(w, `<html><body><h1>Hello %s\n</h1><br /><h2>Below are some of the available APIs for the application</h2><br/><a href="/user_api">User Management API</a><br/><a href="/mileage_api">Mileage Request API</a><br/><a href="/petty_cash_api">Petty Cash API</a><br/><a href="/check_request_api">Check Request API</a></body></html>`, data)
-// }
-
-// func handleUser(code string) ([]byte, error) {
-// 	// Use code to get token and get user info from Google.
-
-// 	token, err := oauthConfig.Exchange(context.Background(), code)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("code exchange wrong: %s", err.Error())
-// 	}
-// 	response, err := http.Get(oauthGoogleUrlAPI + token.AccessToken)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed getting user info: %s", err.Error())
-// 	}
-// 	defer response.Body.Close()
-// 	contents, err := ioutil.ReadAll(response.Body)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed read response: %s", err.Error())
-// 	}
-// 	return contents, nil
-// }
 func main() {
-	maxAge := 86400 * 1
-	isProd := true
-	store := sessions.NewCookieStore([]byte(os.Getenv("COOKIE_SECRET")), []byte(os.Getenv("COOKIE_ENCRYPT")))
-	store.MaxAge(maxAge)
-	store.Options.Path = "/"
-	store.Options.HttpOnly = true
-	store.Options.Secure = isProd
+	// ctx := context.Background()
 
-	gothic.Store = store
-	goth.UseProviders(
-		google.New(os.Getenv("GOOGLE_OAUTH_ID"), os.Getenv("GOOGLE_OAUTH_SECRET"), "https://"+os.Getenv("HEROKU_APP_NAME")+"herokuapp.com/auth/google/callback", "email", "profile"),
-	)
-	http.HandleFunc("/auth/${provider}/callback", func(res http.ResponseWriter, req *http.Request) {
-
-		user, err := gothic.CompleteUserAuth(res, req)
-		if err != nil {
-			fmt.Fprintln(res, err)
-			return
-		}
-		t, _ := template.ParseFiles("templates/success.html")
-		t.Execute(res, user)
-		// fmt.Fprintf(`<html><body><h1>Hello %s\n</h1><br /><h2>Below are some of the available APIs for the application</h2><br/><a href="/user_api">User Management API</a><br/><a href="/mileage_api">Mileage Request API</a><br/><a href="/petty_cash_api">Petty Cash API</a><br/><a href="/check_request_api">Check Request API</a></body></html>`, user)
-	})
-	http.HandleFunc("/auth/{provider}", func(res http.ResponseWriter, req *http.Request) {
-		gothic.BeginAuthHandler(res, req)
-	})
-
-	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		t, _ := template.ParseFiles("templates/index.html")
-		t.Execute(res, false)
-	})
+	// redirectURL := os.Getenv("OAUTH_CALLBACK")
+	// if redirectURL == "" {
+	// 	redirectURL = "https://" + os.Getenv("HEROKU_APP_NAME") + "herokuapp.com/auth/google/callback"
+	// }
+	// config := &oauth2.Config{
+	// 	ClientID:     os.Getenv("GOOGLE_OAUTH_ID"),
+	// 	ClientSecret: os.Getenv("GOOGLE_OAUTH_SECRET"),
+	// 	Endpoint:     google.Endpoint,
+	// 	Scopes:       []string{"email", "profile"},
+	// 	RedirectURL:  redirectURL,
+	// }
+	// authURL := config.AuthCodeURL("state")
+	// fmt.Printf("Follow the link to obtain an auth code: %s", authURL)
+	// token, err := config.Exchange(ctx, "authorization-code")
+	// if err != nil {
+	// 	log.Fatal("config.Exchange: %v", err)
+	// }
+	// client := config.Client(oauth2.NoContext, token)
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
@@ -169,9 +83,6 @@ func main() {
 		GraphiQL:   true,
 		Playground: false,
 	})
-	// http.HandleFunc("/", handleRoot)
-	// http.HandleFunc("/auth/google", handleAuth)
-	// http.HandleFunc("/auth/google/callback", handleAuthCallback)
 	http.Handle("/user_api", userHandler)
 	http.Handle("/mileage_api", mileageHandler)
 	http.Handle("/petty_cash_api", pettyCashHandler)
