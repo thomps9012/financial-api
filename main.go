77df1fd8adc6690971f2b5f90e648204228af1c2
Samuel Thompson <thomps9012@gmail.com>
2022-08-24 12:55:04 -0400

    trimmed down access control origins

--- main.go
+++ main.go
@@ -31,12 +31,12 @@ func main() {
 		Schema:     &rootSchema,
 		Pretty:     true,
 		GraphiQL:   false,
-		Playground: false,
+		Playground: true,
 	})
 	router := chi.NewRouter()
 	router.Use(auth.Middleware())
 	router.Handle("/graphql", rootRequestHandler)
-	originsOK := handlers.AllowedOrigins([]string{"*"})
+	originsOK := handlers.AllowedOrigins([]string{"http://localhost:3000", "https://finance-requests.vercel.app"})
 	headersOK := handlers.AllowedHeaders([]string{"Content-Type", "Authorization", "X-Requested-With"})
 	methodsOK := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"})
 	http.ListenAndServe(":"+port, handlers.CORS(originsOK, headersOK, methodsOK)(router))
