dfe8f4ef5cebbc9576c77343a047207754200b27
Samuel Thompson <thomps9012@gmail.com>
2022-08-25 10:57:35 -0400

    removed playground in favor of encoded json handler

--- main.go
+++ main.go
@@ -4,13 +4,14 @@ import (
 	conn "financial-api/db"
 	r "financial-api/graphql/root"
 	auth "financial-api/middleware"
+	"fmt"
 	"net/http"
 	"os"
-
+	"encoding/json"
 	"github.com/go-chi/chi"
 	"github.com/gorilla/handlers"
 	"github.com/graphql-go/graphql"
-	"github.com/graphql-go/handler"
+	// "github.com/graphql-go/handler"
 )
 
 const defaultPort = "8080"
@@ -20,6 +21,17 @@ var rootSchema, _ = graphql.NewSchema(graphql.SchemaConfig{
 	Mutation: r.RootMutations,
 })
 
+func executeQuery(query string, schema graphql.Schema) *graphql.Result {
+	result := graphql.Do(graphql.Params{
+		Schema: schema,
+		RequestString: query,
+	})
+	if len(result.Errors) > 0 {
+		fmt.Printf("errors: %v", result.Errors)
+	}
+	return result
+}
+
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -27,16 +39,19 @@ func main() {
 	}
 	conn.InitDB()
 	defer conn.CloseDB()
-	rootRequestHandler := handler.New(&handler.Config{
-		Schema:     &rootSchema,
-		Pretty:     true,
-		GraphiQL:   false,
-		Playground: true,
-	})
+	// rootRequestHandler := handler.New(&handler.Config{
+	// 	Schema:     &rootSchema,
+	// 	Pretty:     true,
+	// 	GraphiQL:   false,
+	// 	Playground: false,
+	// })
 	router := chi.NewRouter()
 	router.Use(auth.Middleware())
-	router.Handle("/graphql", rootRequestHandler)
-	originsOK := handlers.AllowedOrigins([]string{"*", "http://localhost:3000", "https://finance-requests.vercel.app"})
+	router.Get("/graphql", func(w http.ResponseWriter, r *http.Request){
+		result := executeQuery(r.URL.Query().Get(("query")), rootSchema)
+		json.NewEncoder(w).Encode(result)
+	})
+	originsOK := handlers.AllowedOrigins([]string{"https://agile-tundra-78417.herokuapp.com/graphql", "http://localhost:3000", "https://finance-requests.vercel.app"})
 	headersOK := handlers.AllowedHeaders([]string{"Content-Type", "Authorization", "X-Requested-With"})
 	methodsOK := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"})
 	http.ListenAndServe(":"+port, handlers.CORS(originsOK, headersOK, methodsOK)(router))
