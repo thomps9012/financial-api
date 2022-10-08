2994f3c6c5e5cb2fdb25f27a96e53aa293346dce
Samuel Thompson <thomps9012@gmail.com>
2022-10-08 10:51:31 -0400

    removed organizational email check for new user creation for public facing api

--- graphql/root/mutation.go
+++ graphql/root/mutation.go
@@ -77,10 +77,6 @@ var RootMutations = graphql.NewObject(graphql.ObjectConfig{
 				if !okEmail {
 					panic(okEmail)
 				}
-				emailCheck, _ := regexp.MatchString("[a-z0-9!#$%&'*+/=?^_{|}~-]*@norainc.org", email)
-				if !emailCheck {
-					panic("users must have a Northern Ohio Recovery Association Email")
-				}
 				name, okName := p.Args["name"].(string)
 				if !okName {
 					panic(okName)
