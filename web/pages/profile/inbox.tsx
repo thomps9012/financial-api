3795e48c6329f85cdf77f73ad16a9ca9ff0f0faa
Samuel Thompson <thomps9012@gmail.com>
2023-04-14 16:20:21 -0400

    stringifying inbox

--- web/pages/profile/inbox.tsx
+++ web/pages/profile/inbox.tsx
@@ -2,10 +2,11 @@ import { useAppContext } from "@/context/AppContext";
 
 export default function ProfileInbox() {
   const { user_profile } = useAppContext();
-  const { name } = user_profile;
+  const { name, incomplete_actions } = user_profile;
   return (
     <main>
       <h1>Inbox Page for {name}</h1>
+      <p>{JSON.stringify(incomplete_actions, null, 2)}</p>
     </main>
   );
 }
